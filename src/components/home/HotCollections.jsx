3b0eea4c80da772868aec52912055146309ebc34
jdblair78 <jdblair78@gmail.com>
2026-04-22 16:21:16 -0400

    swiper carousel setup

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -76,7 +76,7 @@ const HotCollections = () => {
                 }}
               >
                 {collection.slice(0, 6).map((item) => (
-                  <SwiperSlide>
+                  <SwiperSlide key={item.id}>
                     <div className="nft__item">
                       <div className="author_list_pp">
                         <Link to="/author">
@@ -99,7 +99,7 @@ const HotCollections = () => {
                         <Link to="/item-details">
                           <h4>{item.title}</h4>
                         </Link>
-                        <div className="nft__item_price">{item.price} ETH</div>
+                        <div className="nft__item_price">3.08 ETH</div>
                         <div className="nft__item_like">
                           <i className="fa fa-heart"></i>
                           <span>{item.likes}</span>
